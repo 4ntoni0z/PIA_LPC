62a4ef117c0134cf1df20820a3349b3a3e05942f
4ntoni0z <71241570+4ntoni0z@users.noreply.github.com>
2023-11-19 21:58:46 -0600

    Scan_Alive2.ps1
    Escaneo de Puertos en el equipo

---
+++ Script_Powershell/scriptpowershell.ps1
@@ -0,0 +1,50 @@
+
+
+#dterminando el gateway
+$subred= (Get-Netroute -DestinationPrefix 0.0.0.0/0).NextHop
+Write-Host "== Loading ..."
+Write-Host "Tu gateway es:"$subred
+#determinaso el rango del getway
+$rango = $subred.Substring(0,$subred.IndexOf('.')+1+$subred.Substring($subred.IndexOf('.')+1).IndexOf('.')+3)
+echo $rango
+
+$punto=$rango.EndsWith('.')
+if ($punto -like "False") {
+    <# Action to perform if the condition is true #>
+    $rango = $rango + '.'
+
+}
+$rango_ip=@(1..10)
+
+
+Write-Output ""
+Write-Host "-- Subred Actual:"
+Write-Host "Escaneando: " -NoNewline ; Write-Host $rango -NoNewline; Write-Host "0/24" -ForegroundColor Red
+Write-Output ""
+
+foreach( $r in $rango_ip){
+    $actual = $rango+$r
+    $responde = Test-Connection $subred -Quiet -Count 1
+    if ($responde -eq "True") {
+        <# Action to perform if the condition is true #>
+        Write-Output ""
+        Write-Host "host responde: " -NoNewline; Write-Host $actual -ForegroundColor Green
+
+    }
+}
+
+
+$fecha= Get-Date
+Write-Host $fecha
+
+
+
+
+
+
+
+
+
+
+
+
